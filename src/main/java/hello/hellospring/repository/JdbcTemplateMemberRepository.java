669e66b2ee5c7c0197353094f42047d1306741f3
aibig <aih303s@gmail.com>
2022-02-05 01:04:14 +0900

    Mysql auto_increament commit

--- src/main/java/hello/hellospring/repository/JdbcTemplateMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JdbcTemplateMemberRepository.java
@@ -24,6 +24,17 @@ public class JdbcTemplateMemberRepository implements MemberRepository{
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
+    /*
+    SimpleJdbcInsert는 삽입 작업을 간편하게 수행하도록 지원해주는 클래스다.
+    DataSource 혹은 JdbcTemplate으로 SimpleJdbcInsert 객체를 생성할 수 있으며, 이 때 테이블 이름과 자동으로 생성되는 PK 칼럼명을 명시한다.
+    auto_increment 옵션으로 생성된 값이 해당 칼럼명에 자동으로 입력된다.
+    별도의 쿼리문 없이 파라미터들을 Map에 담아 넣어주면 Key값에 해당하는 칼럼명에 Value값을 삽입한다.
+    executeAndReturnKey()는 작업 수행과 동시에 자동 생성된 PK(auto_increment)를 반환한다.
+    모든 DB가 특정 Java 클래스를 반환할 것이라고 의존해서는 안 되며, 해당 메서드는 Number 클래스를 반환한다.
+    이를 원하는 타입으로 적절히 변환해서 사용한다.
+    자동으로 생성(증가)되는 칼럼이 여러 개이거나 숫자 형태가 아닌 경우, executeAndReturnKeyHolder()를 호출하여 KeyHolder를 반환받아 값을 원하는 형태로 적절히 변환한다.
+     */
+
     @Override
     public Member save(Member member) {
         SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
